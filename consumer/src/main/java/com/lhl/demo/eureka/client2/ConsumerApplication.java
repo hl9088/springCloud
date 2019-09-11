a7011acdf3411478def6ffb5e59b570ac6b7e015
李红利 <lihongli@sh.hupu.com>
2019-09-11 11:47:15 +0800

    修改module名称

--- consumer/src/main/java/com/lhl/demo/eureka/client2/ConsumerApplication.java
+++ consumer/src/main/java/com/lhl/demo/eureka/client2/ConsumerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaClient2Application {
+public class ConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClient2Application.class, args);
+        SpringApplication.run(ConsumerApplication.class, args);
     }
 
 }
--- provider/src/main/java/com/lhl/demo/eureka/client/ProviderApplication.java
+++ provider/src/main/java/com/lhl/demo/eureka/client/ProviderApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class EurekaClientApplication {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
 }
